79a55310914bcc82ca9c325dc1c341f3614b9f34
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-09 12:10:20 +0200

    add handleClick to set selectedTopic

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,7 +1,26 @@
 import type { Topic } from "../types";
+import { useTopicsStore } from "../store/topicsStore";
 
 const Word = ({ topic }: { topic: Topic }) => {
-  return <li className="cursor-pointer hover:text-blue-600">{topic.label}</li>;
+  const setSelectedTopic = useTopicsStore((state) => state.setSelectedTopic);
+
+  const handleClick = () => {
+    setSelectedTopic(topic);
+  };
+
+  return (
+    <li
+      onClick={handleClick}
+      className="cursor-pointer hover:text-blue-600"
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") handleClick();
+      }}
+    >
+      {topic.label}
+    </li>
+  );
 };
 
 export default Word;
