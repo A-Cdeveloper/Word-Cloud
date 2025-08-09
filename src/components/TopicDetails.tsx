5d5320b929aabb54f3a6ea0f3d9d157aa30846c6
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-09 12:11:03 +0200

    get selectedTopic and show data in TopicDetails component

--- src/components/TopicDetails.tsx
+++ src/components/TopicDetails.tsx
@@ -1,19 +1,7 @@
-import { useEffect } from "react";
 import { useTopicsStore } from "../store/topicsStore";
 
 const TopicDetails = () => {
-  const setSelectedTopic = useTopicsStore((state) => state.setSelectedTopic);
   const selectedTopic = useTopicsStore((state) => state.selectedTopic);
-
-  useEffect(() => {
-    setSelectedTopic({
-      id: "1751295897__Berlin",
-      label: "Berlin",
-      volume: 165,
-      sentimentScore: 65,
-      sentiment: { negative: 3, neutral: 133, positive: 29 },
-    });
-  }, [setSelectedTopic]);
 
   if (!selectedTopic) {
     return <p>Please select a topic to see details.</p>;
@@ -24,13 +12,11 @@ const TopicDetails = () => {
       <h2 className="text-base mb-2">
         Information on topic: "<strong>{selectedTopic.label}</strong>"
       </h2>
-      <p>
-        <strong>Total Mentions:</strong> {selectedTopic.volume}
-      </p>
+      <p>Total Mentions: {selectedTopic.volume}</p>
       <ul>
-        <li>Positive Mentions: {selectedTopic.sentiment?.positive}</li>
-        <li>Neutral Mentions: {selectedTopic.sentiment?.neutral}</li>
-        <li>Negative Mentions: {selectedTopic.sentiment?.negative}</li>
+        <li>Positive Mentions: {selectedTopic.sentiment?.positive || 0}</li>
+        <li>Neutral Mentions: {selectedTopic.sentiment?.neutral || 0}</li>
+        <li>Negative Mentions: {selectedTopic.sentiment?.negative || 0}</li>
       </ul>
     </div>
   );
