e0b5a52be8eb297ad78790150e9b2b4b9eebbd2a
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-09 17:11:28 +0200

    configure Tailwind for word cloud styling - Add custom sentiment colors (green/red) - Add custom font sizes for word cloud (wc1-wc6)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ export default {
         sentimentRed: "#EF4444",
       },
       fontSize: {
-        wc1: ["0.8125rem", { lineHeight: "1rem" }], // 13px
-        wc2: ["0.875rem", { lineHeight: "1.125rem" }], // 14px
-        wc3: ["1rem", { lineHeight: "1.25rem" }], // 16px
-        wc4: ["1.25rem", { lineHeight: "1.5rem" }], // 20px
-        wc5: ["1.875rem", { lineHeight: "2.25rem" }], // 30px
-        wc6: ["2.5rem", { lineHeight: "2.5rem" }], // 40px
+        wc1: ["1rem", { lineHeight: "1.25rem" }], // ~16px
+        wc2: ["1.25rem", { lineHeight: "1.5rem" }], // ~20px
+        wc3: ["1.5rem", { lineHeight: "1.75rem" }], // ~24px
+        wc4: ["1.875rem", { lineHeight: "2.25rem" }], // ~30px
+        wc5: ["2.25rem", { lineHeight: "2.75rem" }], // ~36px
+        wc6: ["2.5rem", { lineHeight: "3rem" }], // ~40px
       },
     },
   },
