fd715aad143eb2c531a86657b0eb1d046cdc9092
Aleksandar <aleksandar@e-vlasotince.info>
2025-08-09 17:14:16 +0200

    add volume range calculation to WordList set minVolume/maxVolume values in store

--- src/components/WordList.tsx
+++ src/components/WordList.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
 import { useTopicsStore } from "../store/topicsStore";
+import { calculateVolumeRange } from "../utils/calculateVolumeRange";
 import Word from "./Word";
 
 const WordList = () => {
   const topics = useTopicsStore((state) => state.topics);
   const loading = useTopicsStore((state) => state.loading);
   const error = useTopicsStore((state) => state.error);
+
+  const setMinVolume = useTopicsStore((state) => state.setMinVolume);
+  const setMaxVolume = useTopicsStore((state) => state.setMaxVolume);
+
+  useEffect(() => {
+    if (topics.length > 0) {
+      const { minVolume, maxVolume } = calculateVolumeRange(topics);
+      setMinVolume(minVolume);
+      setMaxVolume(maxVolume);
+    }
+  }, [topics, setMinVolume, setMaxVolume]);
 
   if (loading) return <p>Loading topics...</p>;
   if (error) return <p className="text-red-600">{error}</p>;
